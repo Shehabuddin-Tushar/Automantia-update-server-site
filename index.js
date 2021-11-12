2574e0bf47c36c12242ba757a463ae5827cf85b4
Tushar <freelancerrtushar@gmail.com>
2021-11-12 11:31:12 +0600

    some changes

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ async function run(){
         const orders = database.collection("orders");
         const reviews = database.collection("reviews");
        
+        app.post("/addproduct",async(req,res)=>{
+          const productname=req.body.name;
+          const query={name:productname}
+          const findproduct=await products.findOne(query);
+          
+          if(findproduct==null){
+            const result=await products.insertOne(req.body);
+            res.send(result);
+             
+          }else{
+            res.send(false)
+          }
+         });
+
         app.get("/products",async(req,res)=>{
             const result=await products.find({}).toArray();
             res.send(result)
@@ -87,7 +101,6 @@ async function run(){
         app.put("/changestatus/:id",async(req,res)=>{
 
           const id=req.params.id;
-
           const filter = { _id: ObjectId(id) };
           const options = { upsert: true };
           const updateDoc = {
@@ -107,7 +120,6 @@ async function run(){
       app.delete("/deleteproduct/:id",async(req,res)=>{
 
         const id=req.params.id
-
         const query = { _id:ObjectId(id) };
         const result = await products.deleteOne(query);
         if (result.deletedCount === 1) {
@@ -115,7 +127,43 @@ async function run(){
         } else {
           res.send("product not deleted");
         }
-    })
+    });
+
+
+
+    app.put("/makeadmin/:email",async(req,res)=>{
+
+      const email=req.params.email;
+      const filter = { email: email };
+      const result=await users.findOne(filter);
+     if(result==null){
+        res.send(false);
+      }else{
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            role:"admin"
+          },
+         };
+        const result = await users.updateOne(filter, updateDoc, options);
+        if (result.matchedCount === 1) {
+          res.send("Admin created successfully");
+        }
+      }
+     
+     });
+
+     app.get("/userfind/:email",async(req,res)=>{
+            
+           const email=req.params.email;
+           const filter = { email: email };
+           const result=await users.findOne(filter);
+           res.send(result)
+
+
+     })
+
+
 
 
         
