c51922b9fa78d179e6ba63dfa777855e61e2757f
Tushar <freelancerrtushar@gmail.com>
2022-04-02 19:01:08 +0600

    no change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(fileupload())
 
-
+//////////////////////
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lp6z6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
